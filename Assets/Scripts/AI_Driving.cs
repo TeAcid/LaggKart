0c96f66dcb6becf1bcedf22cc5685e4c60777371
Gregor Ivajnsic <gregor.ivajnsic@gmail.com>
2017-11-14 19:00:01 +0100

    Better AI

--- Assets/Scripts/AI_Driving.cs
+++ Assets/Scripts/AI_Driving.cs
@@ -3,16 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AI_Driving : MonoBehaviour {
-
+    [Header("AI Driving")]
     public Transform path;
     public float maxSteerAngle = 40f;
     private List<Transform> nodes;
     private int currentNode = 0;
+    [Header("Wheel Colliders")]
     public WheelCollider wheelFL;
     public WheelCollider wheelFR;
+    public WheelCollider wheelRL;
+    public WheelCollider wheelRR;
+    [Header("Driving and breaking")]
+    public float maxMotorTorque = 60000f;
+    public float currentSpeed;
+    public float maxSpeed = 220f;
+    public float maxBreakTorque = 120000f;
+    public Vector3 centerOfMass;
+    public bool isBreaking = false;
+    [Header("Sensors")]
+    public float sensorLength = 5f;
+
 
     // Use this for initialization
     private void Start () {
+        GetComponent<Rigidbody>().centerOfMass = centerOfMass;
+
         Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
         nodes = new List<Transform>();
 
@@ -27,9 +42,11 @@ public class AI_Driving : MonoBehaviour {
 	
 	// Update is called once per frame
 	private void FixedUpdate () {
+        Sensors();
         ApplySteer();
         Drive();
         CheckWaypointDistance();
+        Braking();
 	}
 
     private void ApplySteer()
@@ -42,8 +59,31 @@ public class AI_Driving : MonoBehaviour {
 
     private void Drive()
     {
-        wheelFL.motorTorque = 60000f;
-        wheelFR.motorTorque = 60000f;
+        currentSpeed = 2 * Mathf.PI * wheelFL.radius * wheelFL.rpm * 60 / 1000;
+
+        if(currentSpeed < maxSpeed && !isBreaking) {
+            wheelFL.motorTorque = maxMotorTorque;
+            wheelFR.motorTorque = maxMotorTorque;
+        }
+        else
+        {
+            wheelFL.motorTorque = 0;
+            wheelFR.motorTorque = 0;
+        }
+    }
+
+    private void Braking()
+    {
+        if (isBreaking)
+        {
+            wheelRL.brakeTorque = maxBreakTorque;
+            wheelRR.brakeTorque = maxBreakTorque;
+        }
+        else
+        {
+            wheelRL.brakeTorque = 0;
+            wheelRR.brakeTorque = 0;
+        }
     }
 
     private void CheckWaypointDistance()
@@ -61,5 +101,20 @@ public class AI_Driving : MonoBehaviour {
                 print("current = " + currentNode);
             }
         }
+    }
+
+    private void Sensors()
+    {
+        RaycastHit hit;
+        Vector3 sensorStartPos = transform.position;
+        //sensorStartPos.z += 0.5f;
+
+        if (Physics.Raycast(sensorStartPos, transform.forward, out hit, sensorLength))
+        {
+            Debug.DrawLine(sensorStartPos, hit.point, Color.red);
+        }
+        //Debug.DrawLine(sensorStartPos, transform.forward, Color.blue);
+        print("sensorStartPos:" + sensorStartPos);
+        //Debug.DrawRay(sensorStartPos, hit.point);
     }
 }
